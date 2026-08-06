2fb425cc74eaabb095fc0353429da9e3808507f1
John Aziz <john.aziz@dtig.us>
2026-08-06 14:09:07 -0500

    Apply stylistic changes to header component

--- header.component.ts
+++ header.component.ts
@@ -1,18 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import * as siteData from '../../data.json'
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import * as siteData from '../../data.json';
 
 @Component({
   selector: 'app-header',
-  standalone: true as boolean,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   headerData: any = (siteData as any).default;
-  constructor() {
-
-  }
-
 }
